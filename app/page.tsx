723b587c81df69cf0da38a04538fcdeb804c3133
yurii macbook pro <yurikorock777@gmail.com>
2026-03-18 22:13:41 +0200

    feat: add header

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="p-10">
-      <h1 className="text-5xl text-red-500 font-bold">
-        Home Page ✅
-      </h1>
-      <Button variant='outline'>Кошик</Button>
+    <div >
+      
+      Home Page
     </div>
   );
 }
---
+++ components/shared/header.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Container } from "./container";
+import Image from "next/image";
+import { Button } from "../ui";
+import { ArrowBigRight, ArrowRight, ShoppingCart, User } from "lucide-react";
+
+interface Props {
+  className?: string;
+}
+
+export const Header: React.FC<Props> = ({ className }) => {
+  return (
+    <header className={cn("border border-b", className)}>
+      <Container className="flex items-center justify-between py-8">
+        {/* Ліва частина */}
+        <div className="flex items-center gap-4">
+          <Image src="/logo.png" alt="Logo" width={35} height={35} />
+          <div>
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">куди вже смачніше</p>
+          </div>
+        </div>
+
+        {/* Права частина */}
+        <div className="flex items-center gap-2">
+          <Button variant="outline" className="flex items-center gap-1">
+            <User size={16} />
+            Увійти
+          </Button>
+          <div>
+            <Button className="group relative ">
+              <b>520 $</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3" />
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"/>
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
