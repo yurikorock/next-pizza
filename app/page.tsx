04a1e3c54584284132d76c8abef1aa6a1854dc65
yurii macbook pro <yurikorock777@gmail.com>
2026-03-19 00:09:31 +0200

    feat: add TopBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import {
+  Categories,
+  Container,
+  SortPopup,
+  Title,
+  TopBar,
+} from "@/components/shared";
 import { Button } from "@/components/ui/button";
-
 
 export default function Home() {
   return (
-    <div >
-      
-      Home Page
-    </div>
+    <>
+      <Container className="mt-10">
+        <Title text="Усі піцци" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+    </>
   );
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,2 +1,16 @@
-export {Container} from './container'
-export {Header} from './header'
+export { Container } from "./container";
+export { Header } from "./header";
+export { Title } from "./title";
+export { Categories } from "./categories";
+export { SortPopup } from "./sort-popup";
+export { TopBar } from "./top-bar";
+
+// import React from "react";
+
+// interface Props {
+//   className?: string;
+// }
+
+// export const TopBar: React.FC<Props> = ({ className }) => {
+//   return <div className={className}></div>;
+// };
