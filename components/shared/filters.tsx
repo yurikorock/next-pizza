a423e44c0e7fd30bc042009acd95d5fe42d78946
yurii macbook pro <yurikorock777@gmail.com>
2026-03-21 00:09:27 +0200

    feat: add slider

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,19 +1,38 @@
 import React from "react";
 import { Title } from "./title";
 import { FilterCheckbox } from "./filter-checkbox";
-
-
+import { Input } from "../ui";
+import { RangeSlider } from "./range-slider";
 
 interface Props {
   className?: string;
 }
 
 export const Filters: React.FC<Props> = ({ className }) => {
-  return <div className={className}>
-    <Title text='Фільтрація' size="sm" className="mb-5 font-bold"/>
-    <div className="flex flex-col gap-4">
-        <FilterCheckbox text='Можна зібрати' value='1'/>
-        <FilterCheckbox text='Новинки' value='2'/>
+  return (
+    <div className={className}>
+      <Title text="Фільтрація" size="sm" className="mb-5 font-bold" />
+
+      <div className="flex flex-col gap-4">
+        <FilterCheckbox text="Можна зібрати" value="1" />
+        <FilterCheckbox text="Новинки" value="2" />
+      </div>
+
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Ціна від і до:</p>
+        <div className="flex gap-3 mb-5">
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={1000}
+            defaultValue={0}
+          />
+          <Input type="number" placeholder="1000" min={100} max={1000} />
+        </div>
+
+        <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
+      </div>
     </div>
-  </div>;
+  );
 };
