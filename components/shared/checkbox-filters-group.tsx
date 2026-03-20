a3b6c54b944afa213f7ebfd527f67c4bb71f4083
yurii macbook pro <yurikorock777@gmail.com>
2026-03-21 01:03:26 +0200

    feat: add checkboxfiltergroup

---
+++ components/shared/checkbox-filters-group.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import React from "react";
+import { FilterChecboxProps, FilterCheckbox } from "./filter-checkbox";
+import { Input } from "../ui";
+
+type Item = FilterChecboxProps;
+
+interface Props {
+  title: string;
+  items: Item[];
+  defaultItems?: Item[];
+  limit?: number;
+  searchInputPlaceholder?: string; //пошук по чекбоксам
+  onChange?: (values: string[]) => void; //які чекбокси вибрали повертає стан
+  defaultValue?: string[]; //які інгредієнти вибрані
+  className?: string;
+}
+
+export const CheckBoxFiltersGroup: React.FC<Props> = ({
+  title,
+  items,
+  defaultItems,
+  limit = 5,
+  searchInputPlaceholder = "Пошук...",
+  className,
+  onChange,
+  defaultValue,
+}) => {
+  return (
+    <div className={className}>
+      <p className="font-bold mb-3"></p>
+
+      <div className="mb-5">
+        <Input
+          placeholder={searchInputPlaceholder}
+          className="bg-gray-50 border-none"
+        />
+      </div>
+
+      <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
+        {items.map((item, index) => (
+          <FilterCheckbox 
+          key={index} 
+          text={item.text} 
+          value={item.value}
+          endAdornment={item.endAdornment}
+          checked={false}
+          onCheckedChange={(ids)=> console.log(ids)}
+          
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -3,6 +3,7 @@ import { Title } from "./title";
 import { FilterCheckbox } from "./filter-checkbox";
 import { Input } from "../ui";
 import { RangeSlider } from "./range-slider";
+import { CheckBoxFiltersGroup } from "./checkbox-filters-group";
 
 interface Props {
   className?: string;
@@ -13,11 +14,13 @@ export const Filters: React.FC<Props> = ({ className }) => {
     <div className={className}>
       <Title text="Фільтрація" size="sm" className="mb-5 font-bold" />
 
+      {/* Верхні чекбокси */}
       <div className="flex flex-col gap-4">
         <FilterCheckbox text="Можна зібрати" value="1" />
         <FilterCheckbox text="Новинки" value="2" />
       </div>
 
+      {/* Фільтр цін */}
       <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
         <p className="font-bold mb-3">Ціна від і до:</p>
         <div className="flex gap-3 mb-5">
@@ -32,6 +35,65 @@ export const Filters: React.FC<Props> = ({ className }) => {
         </div>
 
         <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
+
+        {/* Фільтр інгредієнти */}
+        <CheckBoxFiltersGroup
+          title="Інгредієнти"
+          className="mt-5"
+          limit={6}
+          defaultItems={[
+            {
+              text: "Сирний соус",
+              value: "1",
+            },
+            {
+              text: "Моцарела",
+              value: "2",
+            },
+            {
+              text: "Часник",
+              value: "3",
+            },
+            {
+              text: "Солоні огірки",
+              value: "4",
+            },
+            {
+              text: "Червоний лук",
+              value: "5",
+            },
+            {
+              text: "Томати",
+              value: "6",
+            },
+          ]}
+          items={[
+            {
+              text: "Сирний соус",
+              value: "1",
+            },
+            {
+              text: "Моцарела",
+              value: "2",
+            },
+            {
+              text: "Часник",
+              value: "3",
+            },
+            {
+              text: "Солоні огірки",
+              value: "4",
+            },
+            {
+              text: "Червоний лук",
+              value: "5",
+            },
+            {
+              text: "Томати",
+              value: "6",
+            },
+          ]}
+        />
       </div>
     </div>
   );
