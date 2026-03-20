134a46f7e7dc5804e5c417e505df937d97c381ec
yurii macbook pro <yurikorock777@gmail.com>
2026-03-20 23:51:06 +0200

    feat: add checkbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import {
   Categories,
   Container,
+  Filters,
   SortPopup,
   Title,
   TopBar,
 } from "@/components/shared";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
@@ -13,7 +15,22 @@ export default function Home() {
       <Container className="mt-10">
         <Title text="Усі піцци" size="lg" className="font-extrabold" />
       </Container>
+
       <TopBar />
+
+      <Container className="pb-14">
+        <div className="flex gap-[60px]">
+          {/* Фільтрація */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/* Список товарів */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Список товарів</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
---
+++ components/shared/filters.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Title } from "./title";
+import { FilterCheckbox } from "./filter-checkbox";
+
+
+
+interface Props {
+  className?: string;
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+  return <div className={className}>
+    <Title text='Фільтрація' size="sm" className="mb-5 font-bold"/>
+    <div className="flex flex-col gap-4">
+        <FilterCheckbox text='Можна зібрати' value='1'/>
+        <FilterCheckbox text='Новинки' value='2'/>
+    </div>
+  </div>;
+};
