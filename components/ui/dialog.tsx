2f096c3f9d67ee7ca4fc56dbbd0dbc788c138f4a
yurii macbook pro <yurikorock777@gmail.com>
2026-03-21 18:24:17 +0200

    feat: add prisma

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -72,7 +72,7 @@ function DialogContent({
             <Button
               variant="ghost"
               className="absolute top-2 right-2"
-              size="icon-sm"
+              size="sm"
             >
               <XIcon
               />
