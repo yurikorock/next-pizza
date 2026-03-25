588f496649730245deb71b5a568729f32260fd75
yurii macbook pro <yurikorock777@gmail.com>
2026-03-25 15:58:46 +0200

    style: add to input

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -1,20 +1,43 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { Search } from "lucide-react";
-import React from "react";
+import React, { useRef, useState } from "react";
+import { useClickAway } from "react-use";
 
 interface Props {
   className?: string;
 }
 
 export const SearchInput: React.FC<Props> = ({ className }) => {
+  const [focused, setFocused] = useState(false);
+  const ref = useRef(null);
+
+  useClickAway(ref, () => {
+    setFocused(false);
+  });
+
   return (
-    <div className={cn("flex rounded-2xl flex-1 justify-between relative h-11", className)}>
-      <Search className="absolute top-1/2 translate-y-[-50%] left-3 text-gray-400" />
-      <input
-        className="rounded-2xl outline-none w-full bg-gray-100 pl-11"
-        type="text "
-        placeholder="Знайти піццу"
-      />
-    </div>
+    <>
+      {focused && (
+        <div className="fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30"></div>
+      )}
+
+      <div
+        ref={ref}
+        className={cn(
+          "flex rounded-2xl flex-1 justify-between relative h-11 z-30",
+          className,
+        )}
+      >
+        <Search className="absolute top-1/2 translate-y-[-50%] left-3 text-gray-400" />
+        <input
+          className="rounded-2xl outline-none w-full bg-gray-100 pl-11"
+          type="text "
+          placeholder="Знайти піццу"
+          onFocus={() => setFocused(true)}
+        />
+      </div>
+    </>
   );
 };
