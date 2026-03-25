fd10dd03f2f60d0a7e7159e10b671446362d3eb9
yurii macbook pro <yurikorock777@gmail.com>
2026-03-25 17:07:46 +0200

    fix: edit input and add productsearch

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { Api } from "@/services/api-client";
+import { Product } from "@prisma/client";
 import { Search } from "lucide-react";
-import React, { useRef, useState } from "react";
+import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
 import { useClickAway } from "react-use";
 
 interface Props {
@@ -10,12 +13,20 @@ interface Props {
 }
 
 export const SearchInput: React.FC<Props> = ({ className }) => {
+  const [searchQuery, setSearchQuery] = useState("");
   const [focused, setFocused] = useState(false);
+  const [products, setProducts] = useState<Product[]>([]);
   const ref = useRef(null);
 
   useClickAway(ref, () => {
     setFocused(false);
   });
+
+  useEffect(() => {
+    Api.products.search(searchQuery).then((items) => {
+      setProducts(items);
+    });
+  }, [searchQuery]);
 
   return (
     <>
@@ -36,7 +47,33 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
           type="text "
           placeholder="Знайти піццу"
           onFocus={() => setFocused(true)}
+          value={searchQuery}
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+          }}
         />
+
+        <div
+          className={cn(
+            "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
+            focused && "visible opacity-100 top-12",
+          )}
+        >
+          {products.map((product) => (
+            <Link
+              key={product.id}
+              className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+              href={`/products/${product.id}`}
+            >
+              <img
+                className="rounded-sm h-8 w-8"
+                src={product.imageUrl}
+                alt={product.name}
+              />
+              <span>{product.name}</span>
+            </Link>
+          ))}
+        </div>
       </div>
     </>
   );
---
+++ services/products.ts
@@ -0,0 +1,13 @@
+import { Product } from "@prisma/client";
+import { axiosInstance } from "./instance";
+import { ApiRoutes } from "./constants";
+
+export const search = async (query: string): Promise<Product[]> => {
+  const { data } = await axiosInstance.get<Product[]>(
+    ApiRoutes.SEARCH_PRODUCTS,
+    {
+      params: { query },
+    },
+  );
+  return data;
+};
