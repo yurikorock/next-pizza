e6335ccc74defc9f240e367cd29b121eea2038e1
yurii macbook pro <yurikorock777@gmail.com>
2026-03-30 19:26:53 +0300

    refactor: create hooks use filters, ingredients, query-filters

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -6,79 +6,31 @@ import { FilterCheckbox } from "./filter-checkbox";
 import { Input } from "../ui";
 import { RangeSlider } from "./range-slider";
 import { CheckBoxFiltersGroup } from "./checkbox-filters-group";
-import { useFIlterIngredients } from "@/hooks/useFilterIngredients";
+// import { useFIlterIngredients } from "@/hooks/useFilterIngredients";
 import { useSet } from "react-use";
 import qs from "qs";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useFilters, useIngredients, useQueryFilters } from "@/hooks";
 
 interface Props {
   className?: string;
 }
 
-interface PriceProps {
-  priceFrom?: number;
-  priceTo?: number;
-}
-
-interface QueryFilters extends PriceProps {
-  pizzaTypes: string;
-  sizes: string;
-  ingredients: string;
-}
-
 export const Filters: React.FC<Props> = ({ className }) => {
-  const searchParams = useSearchParams() as unknown as Map<
-    keyof QueryFilters,
-    string
-  >;
-  const router = useRouter();
-  const { ingredients, loading, onAddId, selectedIngredients } =
-    useFIlterIngredients(searchParams.get("ingredients")?.split(","));
+  const { ingredients, loading } = useIngredients();
+  const filters = useFilters();
 
-  const [sizes, { toggle: toggleSizes }] = useSet(
-    new Set<string>(
-      searchParams.has("sizes") ? searchParams.get("sizes")?.split(",") : [],
-    ),
-  );
-  const [pizzaTypes, { toggle: togglePizzaTypes }] = useSet(
-    new Set<string>(
-      searchParams.has("pizzaTypes")
-        ? searchParams.get("pizzaTypes")?.split(",")
-        : [],
-    ),
-  );
-
-  const [prices, setPrice] = useState<PriceProps>({
-    priceFrom: Number(searchParams.get("priceFrom")) || undefined,
-    priceTo: Number(searchParams.get("priceTo")) || undefined,
-  });
+  useQueryFilters(filters);
 
   const items = ingredients.map((item) => ({
     value: String(item.id),
     text: item.name,
   }));
 
-  const updatePrice = (name: keyof PriceProps, value: number) => {
-    setPrice({
-      ...prices,
-      [name]: value,
-    });
+  const updatePrices = (prices: number[]) => {
+    filters.setPrices("priceFrom", prices[0]);
+    filters.setPrices("priceTo", prices[1]);
   };
-
-  useEffect(() => {
-    const filters = {
-      ...prices,
-      pizzaTypes: Array.from(pizzaTypes),
-      sizes: Array.from(sizes),
-      ingredients: Array.from(selectedIngredients),
-    };
-
-    const query = qs.stringify(filters, {
-      arrayFormat: "comma",
-    });
-
-    router.push(`?${query}`, { scroll: false });
-  }, [prices, pizzaTypes, sizes, selectedIngredients, router]);
 
   return (
     <div className={className}>
@@ -90,8 +42,8 @@ export const Filters: React.FC<Props> = ({ className }) => {
         title="Тип тіста"
         name="pizzaTypes"
         className="mb-5"
-        onClickCheckbox={togglePizzaTypes}
-        selected={pizzaTypes}
+        onClickCheckbox={filters.setPizzaTypes}
+        selected={filters.pizzaTypes}
         items={[
           { text: "Тонка", value: "1" },
           { text: "Традиційна", value: "2" },
@@ -102,8 +54,8 @@ export const Filters: React.FC<Props> = ({ className }) => {
         title="Розміри"
         name="sizes"
         className="mb-5"
-        onClickCheckbox={toggleSizes}
-        selected={sizes}
+        onClickCheckbox={filters.setSizes}
+        selected={filters.sizes}
         items={[
           { text: "20 см", value: "20" },
           { text: "30 см", value: "30" },
@@ -120,20 +72,28 @@ export const Filters: React.FC<Props> = ({ className }) => {
             placeholder="0"
             min={0}
             max={1000}
-            value={String(
-              prices.priceFrom !== undefined ? String(prices.priceFrom) : "",
-            )}
-            onChange={(e) => updatePrice("priceFrom", Number(e.target.value))}
+            value={
+              filters.prices.priceFrom !== undefined
+                ? String(filters.prices.priceFrom)
+                : ""
+            }
+            onChange={(e) =>
+              filters.setPrices("priceFrom", Number(e.target.value))
+            }
           />
           <Input
             type="number"
             placeholder="1000"
             min={0}
             max={1000}
-            value={String(
-              prices.priceTo !== undefined ? String(prices.priceTo) : "",
-            )}
-            onChange={(e) => updatePrice("priceTo", Number(e.target.value))}
+            value={
+              filters.prices.priceTo !== undefined
+                ? String(filters.prices.priceTo)
+                : ""
+            }
+            onChange={(e) =>
+              filters.setPrices("priceTo", Number(e.target.value))
+            }
           />
         </div>
 
@@ -141,10 +101,11 @@ export const Filters: React.FC<Props> = ({ className }) => {
           min={0}
           max={1000}
           step={10}
-          value={[prices.priceFrom || 0, prices.priceTo || 1000]}
-          onValueChange={([priceFrom, priceTo]) =>
-            setPrice({ priceFrom, priceTo })
-          }
+          value={[
+            filters.prices.priceFrom || 0,
+            filters.prices.priceTo || 1000,
+          ]}
+          onValueChange={updatePrices}
         />
       </div>
 
@@ -157,8 +118,8 @@ export const Filters: React.FC<Props> = ({ className }) => {
         defaultItems={items.slice(0, 6)}
         items={items}
         loading={loading}
-        onClickCheckbox={onAddId}
-        selected={selectedIngredients}
+        onClickCheckbox={filters.setSelectedIngredients}
+        selected={filters.selectedIngredients}
       />
     </div>
   );
--- hooks/useFilterIngredients.ts
+++ hooks/useFilterIngredients.ts
@@ -1,33 +1,33 @@
-import { Api } from "@/services/api-client";
-import { Ingredient } from "@prisma/client";
-import { useEffect, useState } from "react";
-import { useSet } from "react-use";
+// import { Api } from "@/services/api-client";
+// import { Ingredient } from "@prisma/client";
+// import { useEffect, useState } from "react";
+// import { useSet } from "react-use";
 
-interface ReturnProps {
-  ingredients: Ingredient[];
-  loading: boolean;
-  selectedIngredients: Set<string>;
-  onAddId: (id:string)=> void;
-}
-export const useFIlterIngredients = (values: string[]=[]): ReturnProps => {
-  const [ingredients, setIngredients] = useState<Ingredient[]>([]);
-  const [loading, setLoading] = useState(true);
+// interface ReturnProps {
+//   ingredients: Ingredient[];
+//   loading: boolean;
+//   selectedIngredients: Set<string>;
+//   onAddId: (id:string)=> void;
+// }
+// export const useFIlterIngredients = (values: string[]=[]): ReturnProps => {
+//   const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+//   const [loading, setLoading] = useState(true);
 
-  const [selectedIds, { toggle }] = useSet(new Set<string>(values));
+//   const [selectedIds, { toggle }] = useSet(new Set<string>(values));
 
-  useEffect(() => {
-    async function fetchIngredients() {
-      try {
-        setLoading(true);
-        const ingredients = await Api.ingredients.getAll();
-        setIngredients(ingredients);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchIngredients();
-  }, []);
-  return { ingredients, loading, onAddId: toggle, selectedIngredients:selectedIds };
-};
+//   useEffect(() => {
+//     async function fetchIngredients() {
+//       try {
+//         setLoading(true);
+//         const ingredients = await Api.ingredients.getAll();
+//         setIngredients(ingredients);
+//       } catch (error) {
+//         console.log(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//     fetchIngredients();
+//   }, []);
+//   return { ingredients, loading, onAddId: toggle, selectedIngredients:selectedIds };
+// };
