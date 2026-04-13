91a08f76b3bbe424a65134f60b0b33463e94acd4
yurii macbook pro <yurikorock777@gmail.com>
2026-04-13 18:30:40 +0300

    fix: add @radix-ui/react-visually-hidden

--- shared/components/shared/modals/choose-product-modal.tsx
+++ shared/components/shared/modals/choose-product-modal.tsx
@@ -8,6 +8,10 @@ import { useRouter } from "next/navigation";
 import { ChooseProductForm } from "../choose-product-form";
 import { ProductWithRelations } from "@/@types/prisma";
 import { ChoosePizzaForm } from "../choose-pizza-form";
+import { Root as VisuallyHidden } from "@radix-ui/react-visually-hidden";
+
+
+
 
 interface Props {
   product: ProductWithRelations;
@@ -20,12 +24,15 @@ export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
   const isPizzaForm = Boolean(product.items[0].pizzaType);
   return (
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
-      <DialogContent
+      <DialogContent aria-describedby=""
         className={cn(
           "p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden",
           className,
         )}
       >
+        <VisuallyHidden>
+    <DialogTitle>{product.name}</DialogTitle>
+  </VisuallyHidden>
         {isPizzaForm ? (
           <ChoosePizzaForm
             imageUrl={product.imageUrl}
