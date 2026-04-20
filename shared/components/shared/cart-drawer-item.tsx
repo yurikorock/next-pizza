3501bbec0dfe9f0daae43b54cf182b740366a0c8
yurii macbook pro <yurikorock777@gmail.com>
2026-04-20 21:58:00 +0300

    feat: end create cart-drawer

--- shared/components/shared/cart-drawer-item.tsx
+++ shared/components/shared/cart-drawer-item.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import * as CartItem from "./cart-item-details";
 import { CartItemProps } from "./cart-item-details/cart-item-details.types";
 import { cn } from "@/shared/lib/utils";
+import { CountButton } from "./count-button";
+import { Trash2Icon } from "lucide-react";
 
 interface Props extends CartItemProps {
   className?: string;
@@ -17,12 +19,25 @@ export const CartDrawerItem: React.FC<Props> = ({
   className,
 }) => {
   return (
-    <div className={cn("flex, bg-white p-5 gap-6", className)}>
+    <div className={cn("flex bg-white p-5 gap-6", className)}>
       <CartItem.Image src={imageUrl} />
 
       <div className="flex-1">
+        <CartItem.Info name={name} details={details} />
 
-<CartItem.Info name={name} details={details}/>
+        <hr className="my-3" />
+
+        <div className="flex items-center justify-between">
+          <CountButton onClick={(type) => console.log(type)} value={quantity} />
+
+          <div className="flex items-center gap-3">
+            <CartItem.Price value={price} />
+            <Trash2Icon
+              className="text-gray-400 cursor-pointer hover:text-gray-600 "
+              size={16}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 import {
@@ -13,6 +15,8 @@ import {
 import Link from "next/link";
 import { Button } from "../ui";
 import { ArrowRight } from "lucide-react";
+import { CartDrawerItem } from "./cart-drawer-item";
+import { getCartItemDetails } from "@/shared/lib";
 
 interface Props {
   className?: string;
@@ -26,16 +30,80 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
 
-      <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]" aria-describedby="">
+      <SheetContent
+        className="flex flex-col justify-between pb-0 bg-[#F4F1EE]"
+        aria-describedby=""
+      >
         <SheetHeader>
           <SheetTitle>
             В кошику <span className="font-bold">3 товара</span>
           </SheetTitle>
         </SheetHeader>
 
-        {/* {"Items"} */}
+        <div className="+mx-6 mt-5 overflow-auto flex-1">
+          <div className="mb-2">
+            <CartDrawerItem
+              id={1}
+              imageUrl={
+                "https://media.dodostatic.net/image/r:584x584/0198bf57bc517218ab93c762f4b0193e.avif"
+              }
+              details={getCartItemDetails(2, 30, [
+                {
+                  name: "chiken",
+                  id: 0,
+                  price: 0,
+                  imageUrl: "",
+                  createdAt: new Date(),
+                  updatedAt: new Date(),
+                },
+                {
+                  name: "cheese",
+                  id: 0,
+                  price: 0,
+                  imageUrl: "",
+                  createdAt: new Date(),
+                  updatedAt: new Date(),
+                },
+              ])}
+              name={"Chorizo fresh"}
+              price={419}
+              quantity={1}
+            />
+          </div>
 
-        <SheetFooter className="mx-6 bg-white p-8">
+          <div className="mb-2">
+            <CartDrawerItem
+              id={1}
+              imageUrl={
+                "https://media.dodostatic.net/image/r:584x584/0198bf57bc517218ab93c762f4b0193e.avif"
+              }
+              details={getCartItemDetails(2, 30, [
+                {
+                  name: "chiken",
+                  id: 0,
+                  price: 0,
+                  imageUrl: "",
+                  createdAt: new Date(),
+                  updatedAt: new Date(),
+                },
+                {
+                  name: "cheese",
+                  id: 0,
+                  price: 0,
+                  imageUrl: "",
+                  createdAt: new Date(),
+                  updatedAt: new Date(),
+                },
+              ])}
+              name={"Chorizo fresh"}
+              price={419}
+              quantity={1}
+            />
+          </div>
+          
+        </div>
+
+        <SheetFooter className="+mx-6 bg-white p-8">
           <div className="w-full">
             <div className="flex mb-4">
               <span className="flex flex-1 text-lg text-neutral-500">
