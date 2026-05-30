30d7a4d931ceebf41a9ed1bafb07ebbf3ecf1ab6
yurii macbook pro <yurikorock777@gmail.com>
2026-05-30 19:52:30 +0300

    feat: add header to checkoutlayout

---
+++ app/(checkout)/checkout/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function CheckoutPage({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>Checkout</div>;
+}
--- shared/components/shared/header.tsx
+++ shared/components/shared/header.tsx
@@ -9,12 +9,14 @@ import { SearchInput } from "./search-input";
 import { CartButton } from "./cart-button";
 
 interface Props {
+  hasSearch?: boolean;
+  hasCart?:boolean;
   className?: string;
 }
 
-export const Header: React.FC<Props> = ({ className }) => {
+export const Header: React.FC<Props> = ({ hasSearch = true,hasCart=true, className }) => {
   return (
-    <header className={cn("border border-b", className)}>
+    <header className={cn("border-b", className)}>
       <Container className="flex items-center justify-between py-8">
         {/* Ліва частина */}
         <Link href="/">
@@ -29,7 +31,11 @@ export const Header: React.FC<Props> = ({ className }) => {
           </div>
         </Link>
 
-        <div className="mx-10 flex-1 "><SearchInput/></div>
+        {hasSearch && (
+          <div className="mx-10 flex-1 ">
+            <SearchInput />
+          </div>
+        )}
 
         {/* Права частина */}
         <div className="flex items-center gap-2">
@@ -38,10 +44,7 @@ export const Header: React.FC<Props> = ({ className }) => {
             Увійти
           </Button>
 
-
-         <CartButton/>
-
-
+         {hasCart &&( <CartButton />)}
         </div>
       </Container>
     </header>
