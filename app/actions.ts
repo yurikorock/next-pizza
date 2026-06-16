d961f5d9be8df0bc47d959af83de91ab77d9f57d
yurii macbook pro <yurikorock777@gmail.com>
2026-06-16 18:11:33 +0300

    fix: add create order

--- app/actions.ts
+++ app/actions.ts
@@ -3,26 +3,82 @@
 import { prisma } from "@/prisma/prisma-client";
 import { CheckoutFormValues } from "@/shared/constants/checkout-form-schema";
 import { OrderStatus } from "@prisma/client";
+import { cookies } from "next/headers";
 import { email } from "zod";
 
 export async function createOrder(data: CheckoutFormValues) {
   console.log(data);
+  try {
+    const cookieStore = cookies();
+    const cartToken = (await cookieStore).get("cartToken")?.value;
 
-  const token = "123";
+    if (!cartToken) {
+      throw new Error("Cart token not found");
+    }
 
-  await prisma.order.create({
-    data: {
-      token,
-      totalAmount: 1500,
-      status: OrderStatus.PENDING,
-      items: [],
-      fullName: data.firstName + " " + data.lastName,
-      email: data.email,
-      phone: data.phone,
-      address: data.address,
-      comment: data.comment,
-    },
-  });
+    //знаходимо кошик по токену
+    const userCart = await prisma.cart.findFirst({
+      include: {
+        user: true,
+        items: {
+          include: {
+            ingredients: true,
+            productItem: {
+              include: {
+                product: true,
+              },
+            },
+          },
+        },
+      },
+      where: {
+        token: cartToken,
+      },
+    });
 
-  return 'https://www.google.com/webhp?hl=ru&sa=X&ved=0ahUKEwibj-Siuv2UAxU5QlUIHffTGcYQPAgI'
+    if (!userCart) {
+      throw new Error("Cart not found");
+    }
+
+    if (userCart?.totalAmount === 0) {
+      throw new Error("Cart is empty");
+    }
+
+    // створюємо замовлення
+    const order = await prisma.order.create({
+      data: {
+        token: cartToken,
+        fullName: data.firstName + " " + data.lastName,
+        email: data.email,
+        phone: data.phone,
+        address: data.address,
+        comment: data.comment,
+        totalAmount: userCart.totalAmount,
+        status: OrderStatus.PENDING,
+        items: JSON.stringify(userCart.items),
+      },
+    });
+
+    //очищуємо кошик
+    await prisma.cart.update({
+      where: {
+        id: userCart.id,
+      },
+      data: {
+        totalAmount: 0,
+      },
+    });
+
+    await prisma.cartItem.deleteMany({
+      where: {
+        cartId: userCart.id,
+      }
+    })
+
+    //зробити посилання на оплату
+
+    
+  } catch (error) {}
+
+  return "https://www.google.com/webhp?hl=ru&sa=X&ved=0ahUKEwibj-Siuv2UAxU5QlUIHffTGcYQPAgI";
 }
